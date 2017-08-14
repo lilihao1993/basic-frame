618711f75b9d6e66544c584e424e256bbc4b0ad4
lilihao1993 <18166043874@163.com>
2017-08-14 18:29:44 +0800

    动态扫描 mapper 调整

--- src/main/java/com/basicframe/BasicframeApplication.java
+++ src/main/java/com/basicframe/BasicframeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.basicframe.demo.mapper")
+@MapperScan(basePackages = "com.basicframe.*.mapper")
 public class BasicframeApplication {
 
 	public static void main(String[] args) {
